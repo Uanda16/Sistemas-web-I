33ae17ab3219ba6816ab41fabee568c995a46caf
Uanda16 <studyworkdinha@gmail.com>
2026-09-06 19:13:10 -0300

    .

--- Aula01/Sistemas-web-I/Aula 01/Exer2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$numero = $_POST["numero"];
-
-for($i = 0; $i <= 10; $i++){
-	echo $numero ." x ".$i." = ".($numero*$i)."<br/>";}
-
-?>
--- Aula01/Sistemas-web-I/README.md
+++
@@ -1,2 +0,0 @@
-# Sistemas-web-I
-Atividades do curso 
